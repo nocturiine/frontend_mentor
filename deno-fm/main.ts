b8574325db4e75b8c36c1e0f644c55e65f6f1d44
nocturiine <frankiit@outlook.com>
2025-05-05 22:50:56 -0700

    #1.5 QR Code Component - v1.2 - Routing update

--- deno-fm/main.ts
+++ deno-fm/main.ts
@@ -1,27 +1,27 @@
 import { serveDir } from "@std/http/file-server";
 
-const IMAGES_FOLDER_PATH = "/images";
-const ROUTE_FOLDER_PATH = "/routes";
+const ROUTE_ROOT_NAME = "routes";
 const QR_CODE_COMPONENT_PATH = "/qr-code-component";
 
 function handler(req: Request) {
   const url = new URL(req.url);
   const pathname = decodeURIComponent(url.pathname);
 
-  if (pathname.startsWith(QR_CODE_COMPONENT_PATH)) {
-    if (req.method === "GET") {
-      return serveDir(req, {
-        fsRoot: Deno.cwd().concat(ROUTE_FOLDER_PATH),
-      });
+  if (!pathname.includes(ROUTE_ROOT_NAME)) {
+    if (
+      pathname.startsWith(QR_CODE_COMPONENT_PATH)
+    ) {
+      if (req.method === "GET") {
+        return serveDir(req, {
+          fsRoot: ROUTE_ROOT_NAME,
+        });
+      }
     }
-  }
 
-  if (pathname.startsWith(IMAGES_FOLDER_PATH)) {
     return serveDir(req, {
-      fsRoot: Deno.cwd().concat(IMAGES_FOLDER_PATH),
+      showDirListing: true,
     });
   }
-
   return new Response("404 Not Found", { status: 404 });
 }
 
