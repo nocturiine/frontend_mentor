17e6e53b4d32a2e6dad8f4de8c15421435a1d0a2
Frankii Tang <ftang@CDZ-MBP0551.local>
2025-05-07 17:17:47 -0700

    #3.1 Social links profile - init

--- deno-fm/main.ts
+++ deno-fm/main.ts
@@ -5,9 +5,10 @@ const ROUTES_FS_ROOT = "routes";
 const STATIC_ROUTE = new URLPattern({ pathname: "/static/*"});
 const BLOG_PREVIEW_CARD_ROUTE = new URLPattern({ pathname: "/blog-preview-card*" });
 const QR_CODE_COMPONENT_ROUTE = new URLPattern({ pathname: "/qr-code-component*" });
+const SOCIAL_LINKS_PROFILE_ROUTE = new URLPattern({ pathname: "/social-links-profile*" });
 
 function handler(req: Request) {
-  if(BLOG_PREVIEW_CARD_ROUTE.exec(req.url) || QR_CODE_COMPONENT_ROUTE.exec(req.url)) {
+  if(BLOG_PREVIEW_CARD_ROUTE.exec(req.url) || QR_CODE_COMPONENT_ROUTE.exec(req.url) || SOCIAL_LINKS_PROFILE_ROUTE.exec(req.url)) {
 	if (req.method === "GET") {
         	return serveDir(req, {
           		fsRoot: ROUTES_FS_ROOT, 
